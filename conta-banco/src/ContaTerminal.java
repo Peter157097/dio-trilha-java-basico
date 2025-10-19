9d9b964b93787af3b227148d754d288a9269297f
Peter <112184706+Peter157097@users.noreply.github.com>
2025-10-19 00:04:37 -0300

    Add user input and output for bank account details

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,11 +1,13 @@
 public class ContaTerminal {
     public static void main(String[] args) {
+        //importação e declaração
         Scanner scanner = new Scanner(System.in);
         int numero;
         String agencia;
         String nomeCliente;
         double saldo;
 
+        //entrada de informações
         System.out.println("Digite o número da agencia:");
         agencia = scanner.next();
         System.out.println("Digite o número da conta:");
@@ -15,6 +17,7 @@ public class ContaTerminal {
         System.out.println("Digite o saldo da conta:");
         saldo = scanner.nextDouble();
 
+        //saida
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
 
         scanner.close();
