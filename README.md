ea22488741c645e9692e59942b5f780934845e41
Peter Gomes <peterdeoliveira99@gmail.com>
2025-10-19 00:02:29 -0300

    meu projeto conta banco

--- README.md
+++
@@ -1,2 +0,0 @@
-# dio-trilha-java-basico
-Repositório para colocar os exercícios para o novo curso que estou fazendo no Dio
---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,22 @@
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("Digite o número da agencia:");
+        agencia = scanner.next();
+        System.out.println("Digite o número da conta:");
+        numero = scanner.nextInt();
+        System.out.println("Digite o seu Nome:");
+        nomeCliente = scanner.next();
+        System.out.println("Digite o saldo da conta:");
+        saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
+    }
+}
